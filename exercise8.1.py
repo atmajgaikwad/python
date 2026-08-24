97cb84800a1311fbcd3b46aa6f03c5d4aaa1ef52
atmajgaikwad <atmajgaikwad1447@gmail.com>
2026-08-24 13:57:26 +0530

    Add files via upload

---
+++ exercise8.1.py
@@ -0,0 +1,8 @@
+first_name = input("Enter first name: ").strip()
+last_name = input("Enter last name: ").strip()
+
+full_name = f"{first_name} {last_name}".title()
+
+print("\n----- CLEAN NAME -----")
+print(full_name)
+print("----------------------")
